d59a7474576df5eef18a59b8e9cf20f4d5dc3be6
iclementness <iclementness@gmail.com>
2026-01-30 14:41:27 +0100

    added type events

--- server/src/types/events.ts
+++ server/src/types/events.ts
@@ -1,12 +1,20 @@
+import { UserPublic } from "./users";
+
 export interface Event {
     id: number;
+    created_by: number;
     location: string;
     start_date: Date;
     end_date: Date;
     all_day: boolean;
     title: string;
     description: string;
+    created_at: Date;
+}
+
+export interface EventWithCreator extends Omit<Event, "created_by"> {
+    creator: UserPublic;
 }
 
 export type CreateEventInput = Omit<Event, "created_at" | "id">;
-export type EventPublic = Event;
+export type EventPublic = EventWithCreator;
