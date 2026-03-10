349f530f2343292340464b18b9e51424781990bc
iclementness <iclementness@gmail.com>
2026-03-10 16:57:53 +0100

    feat(client/ROUTER): began integrating routing (and making some shit)

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Dashboard from "./pages/Dashboard";
+import Games from "./pages/Games";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />,
+  },
+  {
+    path: "/games",
+    element: <Games />,
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
