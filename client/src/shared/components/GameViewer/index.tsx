d1063935984ebf0ef4d4d8ee16ecc69b1308a2f1
foufouboy <cissaclement@gmail.com>
2026-03-17 02:49:16 +0100

    feat(client/dashboard): continuing games widget

--- client/src/shared/components/GameViewer/index.tsx
+++ client/src/shared/components/GameViewer/index.tsx
@@ -3,20 +3,21 @@ import { useEffect, useRef } from "react";
 import "./index.sass";
 
 function GameViewer({ pgn }: { pgn: string }) {
-  const viewerRef = useRef<HTMLDivElement>(null);
+	const viewerRef = useRef<HTMLDivElement>(null);
+	console.log(pgn);
 
-  useEffect(() => {
-    if (viewerRef.current) {
-      LichessPgnViewer(viewerRef.current, {
-        pgn,
-        showControls: true,
-        showMoves: false,
-        showPlayers: true,
-      });
-    }
-  }, [pgn]);
+	useEffect(() => {
+		if (viewerRef.current) {
+			LichessPgnViewer(viewerRef.current, {
+				pgn,
+				showControls: true,
+				showMoves: false,
+				showPlayers: true,
+			});
+		}
+	}, [pgn]);
 
-  return <div className="game-viewer" ref={viewerRef}></div>;
+	return <div className="game-viewer" ref={viewerRef}></div>;
 }
 
 export default GameViewer;
